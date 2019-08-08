67651b14b725d9089f563d4066d5d37116c6799e
Mario Lima <v9@outlook.com>
2019-08-08 18:03:52 +0100

    New additions to the structure layout

--- pkg/crawler/crawler.go
+++ pkg/crawler/crawler.go
@@ -6,3 +6,25 @@ import(
 	"github.com/mariolima/repocrawl/pkg/gitlab"
 )
 
+type crawler struct{
+	CrawlerOpts
+	GithubCrawler	*github.GitHubCrawler
+}
+
+type CrawlerOpts struct{
+	GITHUB_ACCESS_TOKEN		string
+}
+
+type Crawler interface{
+
+}
+
+func NewRepoCrawler(opts CrawlerOpts) *crawler {
+	return &crawler{ 
+		opts
+	}
+}
+
+func (c *crawler) GithubCodeSearch(q string) (error) {
+	//TODO
+}
